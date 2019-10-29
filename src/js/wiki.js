0d19b27ee0a4ccb4e6be0fd3a656544d98b013eb
Simon Münker <info@simon-muenker.de>
2019-10-29 17:51:37 +0100

    added wiki javascript enhance

---
+++ src/js/wiki.js
@@ -0,0 +1,13 @@
+class Collapse {
+
+    constructor() {
+
+        this.page = document.getElementById("page");
+        this.collapsibles = this.page.getElementsByClassName("collapse");
+
+    }
+
+    createFader() {
+
+    }
+}
