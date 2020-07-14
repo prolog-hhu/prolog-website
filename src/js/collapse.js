74bf70d8c2cd60dee3964b1ff7c658615bf420ea
simon <simon@simons-MBP.fritz.box>
2020-07-14 17:32:16 +0200

    remove logging

--- src/js/collapse.js
+++ src/js/collapse.js
@@ -1,74 +1,58 @@
 class Collapse {
+  constructor() {
+    this.page = document.getElementById("page");
+    this.collapsibles = this.page.getElementsByClassName("collapse");
 
-    constructor() {
-
-        this.page = document.getElementById("page");
-        this.collapsibles = this.page.getElementsByClassName("collapse");
-
-        for (let item of this.collapsibles) {
-            new Collapsible(item);
-        }
+    for (let item of this.collapsibles) {
+      new Collapsible(item);
     }
+  }
 
+  handleOpen() {}
 
-    handleOpen() {
+  handleClose() {}
 
-    }
-
-
-    handleClose() {
-
-    }
-
-
-    createFader() {
-
-    }
+  createFader() {}
 }
 
 class Collapsible {
+  constructor(elem) {
+    this.elem = elem;
+    this.header = this.elem.getElementsByClassName("header")[0];
+    this.content = this.elem.getElementsByClassName("content")[0];
+    this.button = this.addHandle();
 
-    constructor(elem) {
+    this.content.classList.add("sr-only");
 
-        this.elem = elem;
-        this.header = this.elem.getElementsByClassName('header')[0];
-        this.content = this.elem.getElementsByClassName('content')[0];
-        this.button = this.addHandle();
-        console.log(this.content);
+    this.prepareHeader();
+  }
 
-        this.content.classList.add('sr-only');
+  prepareHeader() {
+    this.header.className +=
+      " d-flex flex-wrap flex-justify-between flex-items-center";
+  }
 
-        this.prepareHeader();
+  addHandle() {
+    let button = document.createElement("button");
+    let text = document.createTextNode("show more");
+    button.appendChild(text);
+
+    button.className += "btn btn-sm btn-outline";
+    button.addEventListener("click", this.handleToggle.bind(this));
+
+    this.header.appendChild(button);
+    return button;
+  }
+
+  handleToggle() {
+    this.content.classList.toggle("sr-only");
+
+    if (!this.content.classList.contains("sr-only")) {
+      this.button.innerHTML = "close";
+    } else {
+      this.button.innerHTML = "show more";
     }
-
-    prepareHeader() {
-        this.header.className += " d-flex flex-wrap flex-justify-between flex-items-center";
-    }
-
-    addHandle() {
-        let button = document.createElement('button');
-        let text = document.createTextNode('show more');
-        button.appendChild(text);
-
-        button.className += 'btn btn-sm btn-outline';
-        button.addEventListener('click', this.handleToggle.bind(this));
-
-        this.header.appendChild(button);
-        return button;
-    }
-
-    handleToggle() {
-
-        this.content.classList.toggle('sr-only');
-
-        if (!this.content.classList.contains('sr-only')) {
-            this.button.innerHTML = 'close'
-
-        } else {
-            this.button.innerHTML = 'show more'
-        }
-
-    }
+  }
 }
 
 let myCollapse = new Collapse();
