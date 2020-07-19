33c0a40d1daf896248120d806adaa907257e5ec8
simon <simon@simons-MBP.fritz.box>
2020-07-19 14:45:58 +0200

    added auto default

--- inc/plugin-customizer.php
+++ inc/plugin-customizer.php
@@ -1,11 +1,16 @@
 <?php
 /**
- * Retrieve support languages
- * @return array Hash of lang => language
- */
-function cl_prolog_get_supported_languages() {
-	
-	$languages = array(
+* Filter the list of supported languages for the Syntax Highlighting colors.
+* Use this filter to shorten or extend the language shown in the editor interface.
+ 
+* The array key must match a Prism's supported alias, the array value is a descriptive
+* field. For languages and aliases see: https://prismjs.com/#supported-languages
+*
+*
+* @param string $languages Array of languages supported
+*/
+add_filter( 'mkaz_code_syntax_language_list', function() {
+    return array(
 		"bash" => "Bash/Shell",
 		"haskell" => "Haskell",
 		"markup" => "HTML",
@@ -14,19 +19,12 @@ function cl_prolog_get_supported_languages() {
         "php" => "PHP",
         "prolog" => "Prolog",
 		"python" => "Python",
-	);
+	);  	
+} );
 
-	/**
-	 * Filter the list of supported languages for the Syntax Highlighting colors.
-	 * Use this filter to shorten or extend the language shown in the editor interface.
-	 
-	 * The array key must match a Prism's supported alias, the array value is a descriptive
-	 * field. For languages and aliases see: https://prismjs.com/#supported-languages
-	 *
-	 * @since 1.1.0
-	 *
-	 * @param string $languages Array of languages supported
-	 */
-	return apply_filters( 'mkaz_code_syntax_language_list', $languages );
-}
+/**
+* Select default language
+*/
+add_filter( 'mkaz_code_syntax_default_lang', function() { return 'prolog'; });
+
 ?>
