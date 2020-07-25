8a4b3eda19b6cbb06cd2becde2bc2f26a77864cc
simon <simon@simons-MBP.fritz.box>
2020-07-25 14:48:27 +0200

    import Quiz, linting

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,9 @@
-import './js/navigation';
-import './js/redirects';
-import './js/prism';
-import './js/wiki';
-import './js/collapse';
+import "./js/navigation";
+import "./js/redirects";
+import "./js/prism";
+import "./js/wiki";
+import "./js/collapse";
 
-import './js/Compiler/';
-import './js/LMS/';
+import "./js/Compiler/";
+import "./js/LMS/";
+import "./js/Quiz/";
