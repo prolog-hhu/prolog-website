a66f77fb8505bb25b08012da24e18af6cb0877e3
simon <simon@simons-MBP.fritz.box>
2020-07-25 14:48:13 +0200

    linting

--- src/js/redirects.js
+++ src/js/redirects.js
@@ -1,22 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let location = window.location.href;
 
-    let location = window.location.href;
+  if (location.match(/search/)) {
+    location = location.replace(/search/, "s");
+    window.location.href = location;
+  }
 
-    if (location.match(/search/)) {
+  if (location.match(/register/)) {
+    location = location.replace(/register/, "registrieren");
+    window.location.href = location;
+  }
 
-        location = location.replace(/search/, 's');
-        window.location.href = location;
-    };
-
-    if (location.match(/register/)) {
-
-        location = location.replace(/register/, 'registrieren');
-        window.location.href = location;
-    };
-
-    if (location.match(/folien-uebersicht/)) {
-
-        location = location.replace(/folien-uebersicht\//, '');
-        window.location.href = location;
-    };
+  if (location.match(/folien-uebersicht/)) {
+    location = location.replace(/folien-uebersicht\//, "");
+    window.location.href = location;
+  }
 });
