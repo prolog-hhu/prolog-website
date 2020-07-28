4a452b0bfa885e73f38609cb5cd5f387d3da1d6f
simon <simon@simons-MacBook-Pro.local>
2020-07-28 14:25:35 +0200

    enable emoji support

--- inc/head-cleanup.php
+++ inc/head-cleanup.php
@@ -38,7 +38,7 @@ remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 
 // Remove Emoji Functions
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
-remove_action('admin_print_scripts', 'print_emoji_detection_script');
-remove_action('admin_print_styles', 'print_emoji_styles');
+// remove_action('wp_head', 'print_emoji_detection_script', 7);
+// remove_action('wp_print_styles', 'print_emoji_styles');
+// remove_action('admin_print_scripts', 'print_emoji_detection_script');
+// remove_action('admin_print_styles', 'print_emoji_styles');
