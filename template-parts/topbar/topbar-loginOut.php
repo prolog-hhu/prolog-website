43b9351d9b7f4e5fc11b93cf39dbd87c6125339a
simon <simon@simons-MBP.fritz.box>
2020-07-30 16:48:32 +0200

    linting and internalisation

--- template-parts/topbar/topbar-loginOut.php
+++ template-parts/topbar/topbar-loginOut.php
@@ -15,9 +15,17 @@ $classes = "text-white border border-gray-dark rounded-2 px-2 py-1 ml-2 ml-md-3 
 
 <?php if (is_user_logged_in()) { ?>
 
-    <a class="<?php echo $classes ?>" href="<?php echo wp_logout_url(); ?>" title="Logout">Logout</a>
+    <a  class="<?php echo $classes ?>" 
+        href="<?php echo wp_logout_url(); ?>" 
+        title="<?php _e('Logout', 'prolog') ?>">
+        <?php _e('Logout', 'prolog') ?>
+    </a>
 
 <?php } else { ?>
 
-    <a class="<?php echo $classes ?>" href="<?php echo wp_login_url(); ?>" title="Login">Login</a>
+    <a  class="<?php echo $classes ?>" 
+        href="<?php echo wp_login_url(); ?>" 
+        title="<?php _e('Login', 'prolog') ?>">
+        <?php _e('Login', 'prolog') ?>
+    </a>
 <?php } ?>
