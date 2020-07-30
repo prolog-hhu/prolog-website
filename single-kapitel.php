1c5af257c21da4233111c51ee0b6a16716880930
simon <simon@simons-MBP.fritz.box>
2020-07-30 16:42:01 +0200

    refractored kapitel template

--- single-kapitel.php
+++ single-kapitel.php
@@ -11,41 +11,33 @@
 
 <?php get_header(); ?>
 
-	<main class="container-xl px-3 py-6">
+<main class="container-xl px-3 py-6">
 
+	<?php
+    if (have_posts()) {
+
+        // Load posts loop.
+        while (have_posts()) {
+            the_post(); ?>
+			
+			<article id="post-<?php the_ID(); ?>">
+
+				<h1 class="mb-4"><?php the_title(); ?></h1>
+
+				<?php get_template_part('template-parts/kapitel/kapitel', 'navigation'); ?>
+
+				<?php the_content(); ?>
+
+			</article>
 		<?php
-        if (have_posts()) {
-
-            // Load posts loop.
-            while (have_posts()) {
-                the_post(); ?>
-				
-				<article id="post-<?php the_ID(); ?>">
-
-					<h1 class="mb-4"><?php the_title(); ?></h1>
-
-					<div class="container mb-4">
-
-					    <a class="btn btn-primary mb-3 mr-3" href="<?php the_field('quiz_anfang') ?>" target="_blank">Zu den Aufgaben</a>
-
-						<a class="btn btn-outline mb-3 mr-3" href="<?php the_field('pdf_folien') ?>" target="_blank">Folien ansehen</a>
-
-						<?php if (get_field('musterlosung') != '' && get_field('musterlosung_anzeigen') == true): ?>
-							<a class="btn btn-invisible mb-3 mr-3" href="<?php the_field('musterlosung') ?>">Musterlösungen herunterladen</a>
-						<?php endif; ?>
-					</div>
-
-					<?php the_content(); ?>
-
-				</article>
-			<?php
-            }
-        } else {
-
-            // If no content, include the "No posts found" template.
-            get_template_part('template-parts/content/content', 'none');
         }
-        ?>
+    } else {
 
-	</main>
+        // If no content, include the "No posts found" template.
+        get_template_part('template-parts/content/content', 'none');
+    }
+    ?>
+
+</main>
+
 <?php get_footer(); ?>
---
+++ template-parts/kapitel/kapitel-navigation.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Template part for displaying the quiz navigation
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @since 2.0.0
+ */
+
+$aufgabenAnfang = get_field('quiz_anfang');
+$pdfFolien = get_field('pdf_folien');
+$musterloesung = get_field('musterlosung');
+$musterloesungAnzeigen = get_field('musterlosung_anzeigen');
+
+?>
+
+<div class="container mb-4">
+
+    <?php // conditional output aufgaben anfang
+    if (!empty($aufgabenAnfang)) { ?>
+
+        <a  class="btn btn-primary mb-3 mr-3"
+            href="<?php echo esc_url($aufgabenAnfang); ?>"
+            target="_blank">
+                <?php _e('Zu den Aufgaben', 'prolog'); ?>
+        </a>
+        
+    <?php
+    // conditional output PDF Folien
+    } if (!empty($pdfFolien)) { ?>
+
+        <a  class="btn btn-outline mb-3 mr-3"
+            href="<?php echo esc_url($pdfFolien); ?>"
+            target="_blank">
+                <?php _e('Folien ansehen', 'prolog'); ?>
+        </a>
+
+    <?php
+    }
+    // conditional output Musterlösung
+    if (!empty($musterloesung) && $musterloesungAnzeigen == true) { ?>
+
+        <a  class="btn btn-invisible mb-3 mr-3"
+            href="<?php echo esc_url($musterloesung); ?>"
+            target="_blank">
+                <?php _e('Musterlösungen herunterladen', 'prolog'); ?>
+        </a>
+
+    <?php
+    } ?>
+
+</div>
