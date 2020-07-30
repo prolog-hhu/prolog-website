0fe8ab75a85627a3e57b316c1df7311544cce760
simon <simon@simons-MBP.fritz.box>
2020-07-30 15:07:31 +0200

    updated quiz navigation according to meeting

--- template-parts/quiz/quiz-navigation.php
+++ template-parts/quiz/quiz-navigation.php
@@ -9,13 +9,19 @@
  * @since 2.0.0
  */
 
+$nextTask = get_field('next_task');
+$nextChapter = get_field('next_chapter');
+
 ?>
 
 <div class="gutter d-flex flex-wrap flex-justify-center flex-items-center">
     <a href="#" class="btn btn-blue mr-2">
-        > Nächste Aufgabe
+        < Zum letzten Schritt
     </a>
-    <a href="#" class="btn btn-grey">
-        >> Nächstes Kapitel
+    <a href="<?php echo esc_url($nextTask['url']); ?>" class="btn btn-blue mr-2">
+        > Nächster Schritt
+    </a>
+    <a href="<?php echo esc_url($nextChapter['url']); ?>" class="btn btn-grey">
+        >> Direkt Programmieren
     </a>
 </div>
