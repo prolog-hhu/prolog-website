436fc0c4065f66bf4eff2f3d0d242e7960dacf09
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-08-05 12:04:18 +0200

    included dropdown/input answer template #13 #14

--- template-parts/quiz/quiz.php
+++ template-parts/quiz/quiz.php
@@ -12,6 +12,9 @@
 the_row();
 $question = get_sub_field('question');
 
+$questionType = $question['type'];
+$questionDescription = $question['description'];
+
 ?>
 
 <form class="quiz mb-4" action="#">
@@ -19,13 +22,23 @@ $question = get_sub_field('question');
     <?php
 
     # show question
-    echo $question['description'];
+    echo $questionDescription;
 
-    # loop through each answer
-    if (have_rows('answer')) {
+    # display answer type choice
+    if (have_rows('answer') && strcmp($questionType, 'choice') == 0) {
         while (have_rows('answer')) {
             get_template_part('template-parts/quiz/answer/answer', 'choice');
         }
+    }
+
+    # display answer type dropdown
+    elseif (have_rows('answer') && strcmp($questionType, 'dropdown') == 0) {
+        get_template_part('template-parts/quiz/answer/answer', 'dropdown');
+    }
+    
+    # display answer type input
+    elseif (have_rows('answer') && strcmp($questionType, 'input') == 0) {
+        get_template_part('template-parts/quiz/answer/answer', 'input');
     }
     
     ?>
