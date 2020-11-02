cc503a358641154f886cbfaa47fdab9922107c5a
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-11-02 14:31:32 +0100

    removed login from topbar, added tutorial link

--- template-parts/topbar/topbar.php
+++ template-parts/topbar/topbar.php
@@ -18,13 +18,13 @@
 
             <div class="d-flex flex-justify-between flex-items-center">
 
-                <?php
-                    
-                    get_search_form();
+                <?php get_search_form(); ?>
 
-                    get_template_part('template-parts/topbar/topbar', 'loginOut');
-
-                ?>
+                <a  class="text-white border border-gray-dark rounded-2 px-2 py-1 ml-2 ml-md-3 hide-sm" 
+                    href="https://prolog.cl.phil.hhu.de/tutorial/" 
+                    title="Zum Tutorial">
+                        Tutorial
+                </a>
 
                 <button id="open-navigation" class="btn-link text-white ml-2 ml-md-4" aria-label="open navigation">
                     <?php get_template_part('template-parts/octicon/octicon', 'nav32'); ?>
