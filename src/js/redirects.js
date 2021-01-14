f70eb9365ed9b820683833d432126e4d48008a3e
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2021-01-14 14:21:13 +0100

    redirect to alternative hosting plattform

--- src/js/redirects.js
+++ src/js/redirects.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   let location = window.location.href;
 
+  // tmp redirect to alternative server
+  if (location.match("https://prolog.cl.phil.hhu.de/")) {
+    location = location.replace(
+      "https://prolog.cl.phil.hhu.de/",
+      "https://prolog.xciv.de/"
+    );
+    window.location.href = location;
+  }
+
   if (location.match(/search/)) {
     location = location.replace(/search/, "s");
     window.location.href = location;
